09034a33be97ba6d5cdda127bd7d4ec050ca4fcf
kavindasilva <kss.kavinda@gmail.com>
2018-03-28 21:13:26 +0530

    long press not working

--- src/pages/pg3/pg3.ts
+++ src/pages/pg3/pg3.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {AlertController} from 'ionic-angular';
+
+//import { Directive, ElementRef, OnInit, OnDestroy, Output, EventEmitter } from '@angular/core';
+//import { Gesture } from "ionic-angular/gestures/gesture";
 
 /**
  * Generated class for the Pg3Page page.
@@ -8,29 +12,59 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
+ @IonicPage()
 @Component({
   selector: 'page-pg3',
   templateUrl: 'pg3.html',
 })
+
+//@Output('long-press') onPressRelease: EventEmitter<any> = new EventEmitter();
+//el: HTMLElement;
+//pressGesture: Gesture;
+//@Output('long-press') onPressRelease: EventEmitter<any> = new EventEmitter();
+
 export class Pg3Page {
+  /*el: HTMLElement;
+  pressGesture: Gesture;
+
+  @Output('long-press') onPressRelease: EventEmitter<any> = new EventEmitter();*/
 
   movie: {};
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
 
+  //constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alertController: AlertController) {
     this.movie = navParams.get('movie');
   }
+  
 
   goback() {
     this.navCtrl.pop();
     console.log('Click on button Test Console Log');
  }
 
+ //alert sample code
+ openFilters() {
+  let alert = this.alertController.create({
+      title: 'Example',
+      subTitle: 'Example subtitle',
+      buttons: ['OK']
+  });
+
+    alert.present();
+  }
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad Pg3Page');
   }
+/*
+  longTest(){
+    this.openFilters();
 
+    this.pressGesture.on('press', (event) => {
+      this.onPressRelease.emit('released');
+    });
+  }*/
 
   itemTapped(event, movie) {
 		console.log(movie);  
